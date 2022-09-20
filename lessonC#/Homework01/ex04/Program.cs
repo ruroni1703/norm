ab69f263a530e5ecb726c78b0953ab985d0b7acf
nikita <nik_xxx_@mail.ru>
2022-09-20 14:00:19 +0500

    seminar02ex01

--- lessonC#/Homework01/ex04/Program.cs
+++ lessonC#/Homework01/ex04/Program.cs
@@ -2,12 +2,12 @@
 int n = Convert.ToInt32(Console.ReadLine());
 int i = 1;
 
-while (i < n+1)
+while (i < n + 1)
 {
-      if (i % 2 ==0)
-      Console.WriteLine(i);
-      i++;
-    
+    if (i % 2 == 0)
+        Console.WriteLine(i);
+    i++;
+
 }
 
 // или так
---
+++ lessonC#/Seminar02/ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу которая выводит случайное число из отрезка [10,99] 
+и показывает наибольшую цифру этого числа
+78-8
+12-2 */
+
+Console.Clear();
+int n = new Random().Next(10, 100);
+Console.WriteLine("Загадано число :");
+Console.WriteLine(n);
+int a = n / 10;
+int b = n % 10;
+
+Console.WriteLine("Максимальное цифра");
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
