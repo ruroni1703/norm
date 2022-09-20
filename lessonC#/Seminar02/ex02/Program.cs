0879593a000e9930dc747f31c7efd5319802de4e
nikita <nik_xxx_@mail.ru>
2022-09-20 14:47:49 +0500

    sem02ex02 исходная программа

---
+++ lessonC#/Seminar02/ex02/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Определить присутствует ли в заданном массиве
+некоторое число */
+
+int count = 10;
+int[] array = new int[count];
+int find = new Random().Next(1, 21);
+int index = 0;
+
+while (index < count)
+{
+    array[index] = new Random().Next(1, 101);
+    index++;
+}
+
+index = 0;
+
+while (index < count)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine("ДА");
+    }
+    index++;
+}
+
+Console.WriteLine("END");
