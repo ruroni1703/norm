87c523f8da3046034c17418497224297f20f1e94
Nikita <nikitafox770@gmail.com>
2022-09-29 23:56:55 +0500

    31

--- lessonC#/Homework03/ex03/Program2.cs
+++ lessonC#/Homework03/ex03/Program2.cs
@@ -1,71 +1,71 @@
-// /* Задача 23
-// Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// // /* Задача 23
+// // Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125   решение подсказали*/  
+// // 3 -> 1, 8, 27
+// // 5 -> 1, 8, 27, 64, 125   решение подсказали*/  
 
-int number;
+// int number;
 
-// метод получения числа
-int GetIntNum(int number)
-{
-    Console.Write("Введите элемент массива: ");
-    number = Convert.ToInt32(Console.ReadLine());
-    return number;
-}
+// // метод получения числа
+// int GetIntNum(int number)
+// {
+//     Console.Write("Введите элемент массива: ");
+//     number = Convert.ToInt32(Console.ReadLine());
+//     return number;
+// }
 
- // создаем массив( с указанным количеством элементов)
-int[] CreateArray(int size)
-{
-    int[] array = new int[size];
-    return array;
-}
+//  // создаем массив( с указанным количеством элементов)
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+//     return array;
+// }
 
-//  метод заполнения массива 
-void FillArray(int[] arr)
-{
-    int i = 0;
-    while (i < arr.Length)
-    {
-        arr[i] = GetIntNum(number);
-        i++;
-    }
-}
-
-
-//получение кубов чисел
-int[] Create(int[] el)
-{
-    int temp = el.Length;
-    for (int i = 0; i < el.Length; i++)
-    {
-        el[i] = el[i] * el[i] * el[i];
-    }
-    return el;
-}
+// //  метод заполнения массива 
+// void FillArray(int[] arr)
+// {
+//     int i = 0;
+//     while (i < arr.Length)
+//     {
+//         arr[i] = GetIntNum(number);
+//         i++;
+//     }
+// }
 
 
+// //получение кубов чисел
+// int[] Create(int[] el)
+// {
+//     int temp = el.Length;
+//     for (int i = 0; i < el.Length; i++)
+//     {
+//         el[i] = el[i] * el[i] * el[i];
+//     }
+//     return el;
+// }
 
 
-Console.Write("Введите число n: ");
-number = Convert.ToInt32(Console.ReadLine());
 
 
-// Здесь мы создаем массив arr из number элементов, пока он будет заполнен нулями!
-int[] arr = CreateArray(number);
-// Заполняем массив данными с клавиатуры
-FillArray(arr);
-// Выводим массив в консоль
-for(int i = 0; i < arr.Length; i++) {
-    Console.Write($"{arr[i]}\t|"); Console.WriteLine($"Таблица чисел от 1 до {number}");
-}
-Console.WriteLine("\b"); // форматирование вывода
+// Console.Write("Введите число n: ");
+// number = Convert.ToInt32(Console.ReadLine());
 
-// Теперь нам остается ввести новый массив и вызвать метод Create;
-int[] newArr = Create(arr);
-for(int i = 0; i < newArr.Length; i++) 
-{
+
+// // Здесь мы создаем массив arr из number элементов, пока он будет заполнен нулями!
+// int[] arr = CreateArray(number);
+// // Заполняем массив данными с клавиатуры
+// FillArray(arr);
+// // Выводим массив в консоль
+// for(int i = 0; i < arr.Length; i++) {
+//     Console.Write($"{arr[i]}\t|"); Console.WriteLine($"Таблица чисел от 1 до {number}");
+// }
+// Console.WriteLine("\b"); // форматирование вывода
+
+// // Теперь нам остается ввести новый массив и вызвать метод Create;
+// int[] newArr = Create(arr);
+// for(int i = 0; i < newArr.Length; i++) 
+// {
     
-    Console.Write($"{newArr[i]}\t|"); Console.WriteLine($"Таблица кубов от 1 до {number}");
-}
-Console.WriteLine("\b");
+//     Console.Write($"{newArr[i]}\t|"); Console.WriteLine($"Таблица кубов от 1 до {number}");
+// }
+// Console.WriteLine("\b");
---
+++ lessonC#/seminar05/ex31/Program.cs
@@ -0,0 +1,67 @@
+﻿/* /* Задача 31: Задайте массив из 12 элементов, заполненный случайными
+ числами из промежутка [-9, 9]. Найдите сумму отрицательных и
+  положительных элементов массива.
+===
+
+int  */
+
+// / 1 создать массив из 12 элементов
+// 2 заполнить случайными числами
+// печать
+// найти сумму положительных
+// найти сумму отрицательных
+// найти их сумму
+
+
+
+Console.Clear();
+int[] FillArray()
+{
+    int[] arr = new int[12];
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+                
+    }
+    return arr;
+}
+int SumP(int[] sp)
+{
+    int suP = 0;
+    for (int i = 0; i < sp.Length; i++)
+    {
+        if (sp[i] > 0)
+        {
+            suP += sp[i]; // sumP = sumP + array[i];
+        }
+
+    }
+    return suP;
+
+}
+int SumN(int[] sn)
+{
+    int suN = 0;
+    for (int i = 0; i < sn.Length; i++)
+    {
+        if (sn[i] < 0)
+        {
+            suN += sn[i];
+        }
+    }
+    return suN;
+}
+
+int[] mass = FillArray();
+int SummP = SumP(mass);
+int SummN = SumN(mass);
+
+Console.WriteLine("Создан массив из 12 элементов");
+Console.WriteLine(string.Join(", ", mass));
+Console.WriteLine($"сумма положительных чисел = {SummP}");
+
+Console.WriteLine($"сумма отрицательных чисел = {SummN}");
+
+
+
