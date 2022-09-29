58fd5ed063b8a842597eebaad0cc95d5754bc5d4
nikita <nik_xxx_@mail.ru>
2022-09-29 16:28:52 +0500

    29.09

--- lessonC#/Homework03/ex02/Program.cs
+++ lessonC#/Homework03/ex02/Program.cs
@@ -7,6 +7,9 @@ A (3,6,8); B (2,1,-7), -> 15.84
 
 A (7,-5, 0); B (1,-1,9) -> 11.53 */
 
+// через метод я не придумал как.....
+
+
 Console.WriteLine("Введите координату x для первой точки : ");
 int xa = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату y для первой точки : ");
@@ -20,5 +23,11 @@ int yb = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату z для второй точки : ");
 int zb = Convert.ToInt32(Console.ReadLine());
 
-double X = Math.Sqrt(Math.Pow(xb - xa,2) + Math.Pow(yb - ya,2) + Math.Pow(zb - za,2));
+
+
+
+
+double X = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+
+
 Console.WriteLine($"Расстояние между точками равно {X} ");
