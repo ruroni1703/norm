e66fc8d9ce83af692208a1fcb9256f57992b7c4b
Nikita <nikitafox770@gmail.com>
2022-10-12 23:26:33 +0500

    12

---
+++ lessonC#/Seminar07/ex54/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 54: Задайте двумерный массив.
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] matrix = new int[3, 4]
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+
+int n = 4;
+int m = 3;
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+for (int k = 0; k < m; k++)
+{
+    for (int i = 0; i < n; i++)    // сортирует одну строчку массива,чтобы и столбцы добавляем k и еще один цимкл
+    {
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (matrix[k, j] < matrix[k, j + 1])
+            {
+                int temp = matrix[k, j];
+                matrix[k, j] = matrix[k, j + 1];
+                matrix[k, j + 1] = temp;
+            }
+        }
+    }
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < m; i++)   // печать
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ lessonC#/ZADACHI/ex47/Program.cs
@@ -0,0 +1,39 @@
+﻿// 47 Написать программу которая будет создавать копию заданного массива
+// с помощью поэлементного копирования
+
+void Zadacha47()
+{
+    int size = 10;
+    int[] numbers = new int[size];
+    int[] coppynumbers = new int[size];
+    FillArray(numbers, -10, 10);
+    PrintArray(numbers);
+    for (int i = 0; i < size; i++)
+    {
+        coppynumbers[i] = numbers[i];
+    }
+    PrintArray(coppynumbers);
+
+}
+
+void FillArray(int[] numbers, int minValue = 0, int maxValue = 9)
+{
+    maxValue++;
+    int size = numbers.Length;
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = random.Next(minValue, maxValue);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.WriteLine("Вывод массива ");
+    Console.WriteLine("[" + String.Join(",", numbers) + "]");
+
+}
+
+
+
+Zadacha47();
